dbe077a6096194899db400cbbd32b50123b1fd64
Sateesh Mandava <sateeshm@gmail.com>
2015-03-23 23:28:54 -0400

    removed hardcoded paths

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 ﻿
 # Load posh-git example profile
-. 'C:\Users\smandava\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+. "$(Split-Path $MyInvocation.MyCommand.Path -Parent)\Modules\posh-git\profile.example.ps1"
 
